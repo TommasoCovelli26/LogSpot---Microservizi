dd29bd35699977b1601080c9fae092b99af8b457
TommasoCovelli26 <151685551+TommasoCovelli26@users.noreply.github.com>
2026-07-05 12:56:30 +0200

    Salvataggio temporaneo

--- gateway-service/src/main/java/it/logspot/gatewayservice/config/SecurityConfig.java
+++ gateway-service/src/main/java/it/logspot/gatewayservice/config/SecurityConfig.java
@@ -1,4 +1,22 @@
 package it.logspot.gatewayservice.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(csrf -> csrf.disable()) // Disabilita il blocco CSRF per permettere chiamate POST/PUT
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll() // Per ora fa passare TUTTI senza login
+                );
+        return http.build();
+    }
 }
